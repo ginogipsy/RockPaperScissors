33599f40ca94f40bdfdc50540ae8bca0233f45cd
ginogipsy <francescogrossi.work@gmail.com>
2026-01-12 16:05:36 +0100

    (#003) project 1.1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,21 +2,17 @@ package com.ginogipsy
 
 import kotlin.system.exitProcess
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-  var computerChoice = ""
-  var playerChoice = ""
   println("Rock, Paper or Scissors? Enter your choice!")
 
-  var count : Int = 0
+  var count = 0
   val validChoices = setOf("Rock", "Paper", "Scissors")
 
-  do  {
+  val playerChoice = generateSequence {
     count++
 
     when (count) {
-      2 -> println("You must choice Rock, Paper or Scissors")
+      2 -> println("You must choose Rock, Paper or Scissors")
       3 -> println("Rock, Paper or Scissors!!!!!!!")
       4 -> {
         println("It's enough! You're a jackass!")
@@ -24,31 +20,19 @@ fun main() {
       }
     }
 
-    playerChoice = readln()
-  } while (isChoiceNotCorrect(playerChoice))
+    readln()
+  }.first { it in validChoices }
 
-  val randomNumber = (1..3).random()
+  val computerChoice = listOf("Rock", "Paper", "Scissors").random()
+  println("Computer chose: $computerChoice")
 
-    when (randomNumber) {
-      1 -> computerChoice = "Rock"
-      2 -> computerChoice = "Paper"
-      3 -> computerChoice = "Scissors"
-    }
-
-    println(computerChoice)
-
-    val winner = when {
-      playerChoice == computerChoice -> "Tie"
-      playerChoice == "Rock" && computerChoice == "Scissors" -> "Player"
-      playerChoice == "Scissors" && computerChoice == "Paper" -> "Player"
-      playerChoice == "Paper" && computerChoice == "Rock" -> "Player"
-      else -> "Computer"
-
-    }
+  val winner = when {
+    playerChoice == computerChoice -> "Tie"
+    playerChoice == "Rock" && computerChoice == "Scissors" -> "Player"
+    playerChoice == "Scissors" && computerChoice == "Paper" -> "Player"
+    playerChoice == "Paper" && computerChoice == "Rock" -> "Player"
+    else -> "Computer"
+  }
 
   println("Winner: $winner")
-}
-
-fun isChoiceNotCorrect(playerChoice: String): Boolean {
-  return playerChoice != "Rock" && playerChoice != "Paper" && playerChoice != "Scissors"
 }
